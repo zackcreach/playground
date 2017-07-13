7722d0ea61f4ade157aeba5276180b3bfaa70727
zackcreach <zack@peachdish.com>
2017-07-13 11:14:12 -0400

    Up and running with test 2.

--- app/test2.js
+++ app/test2.js
@@ -1,5 +1,34 @@
-const test2 = (str) => {
-  console.log(str);
+
+/*
+ * Complete the function below.
+ */
+
+ function tripleThreat(a) {
+
+ };
+
+ 
+
+ var a = [222, 5, 4, 6, 10, 44];
+
+
+var sorted = a.sort(function(a, b) {
+  if(a > b) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+var result = function() {
+  if(result.includes()) {
+    return 1;
+  } else {
+    return 0;
+  }
 };
+
+console.log(result);
+
 
 export default test2;

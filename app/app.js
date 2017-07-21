959caf9df5328ea6fb245532575d656b584cd027
zackcreach <zackcreach@gmail.com>
2017-07-21 16:15:12 -0400

    Added tests.

--- app/app.js
+++ app/app.js
@@ -10,9 +10,36 @@
 
 
 // Selector fun
-import selected from 'selectors';
+// import selected from 'selectors';
+//
+// const colorAry = ['blue', 'red', 'yellow', 'black', 'purple'];
+// const appList = document.querySelector('app');
+//
+// new selected(colorAry, appList);
 
-const colorAry = ['blue', 'red', 'yellow', 'black', 'purple'];
-const appList = document.querySelector('app');
 
-new selected(colorAry, appList);
+// Test
+var x = 6;
+var y = 4;
+var a = function(b) {
+  return function(c) {
+    return y + b + c;
+  }
+};
+x = 2;
+y = 5;
+var fn = a(x);
+x = 1;
+y = 3;
+var unknown = 5;
+console.log(fn(unknown));
+
+
+
+function scaleImage(width, height, maxdim) {
+var scale = Math.min(maxdim / width, maxdim / height);
+
+return [scale * width, scale * height];
+}
+
+console.log(scaleImage(2000,44080,500));
